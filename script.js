0fda874401ed69da096ea0244e5cde785e66207a
Stephen Mayeux <stephenmayeux@gmail.com>
2018-05-24 17:11:32 -0500

    async version

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
-const buffer = fs.readFileSync('./poem.txt');
-const result = buffer.toString();
-console.log(result);
+// Synchronous Method of reading files
+// const buffer = fs.readFileSync('./poem.txt');
+// const result = buffer.toString();
+// console.log(result);
+
+fs.readFile('./poem.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log('There was an error:', err);
+  } else {
+    console.log(data);
+  }
+});
 
 console.log('The end of the script...');
